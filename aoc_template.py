cf0a0ecc01e844cad8cf73c1a97a96b0f8e25e79
LeonardoE95 <leonardotamiano95@gmail.com>
2021-12-06 13:39:30 +0100

    Added aoc template for python

---
+++ aoc_template.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def part_one():
+    with open("input.txt", "r") as f:
+        pass
+
+# ------    
+    
+def part_two():
+    with open("input.txt", "r") as f:
+        pass
+
+# ------
+    
+if __name__ == "__main__":
+    part_one()
+    part_two()
