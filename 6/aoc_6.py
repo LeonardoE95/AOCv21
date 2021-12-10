06a3f4f470eaadfbfa9049f5d5fea177405c3aae
LeonardoE95 <leonardotamiano95@gmail.com>
2021-12-10 01:56:47 +0100

    Improved code for day 06

--- 6/aoc_6.py
+++ 6/aoc_6.py
@@ -14,86 +14,58 @@
 # Find a way to simulate lanternfish. How many lanternfish would there
 # be after 80 days?
 
-def print_population(population):
-    keys = list(population.keys())
-    keys.sort()
-
-    for k in keys:
-        if population[k] > 0:
-            print((str(k) + " ") * population[k], end="")
-    print()
-
-# ------
 
 def part_one():
-    # population[i] := number of lanternfish that will reproduce after i days.
-    population = {}
-    
+    population = [0] * 9
+
     with open("input.txt", "r") as f:
+        # -- read input
         for n in f.read().split(","):
-            population[int(n)] = 1 if int(n) not in population else population[int(n)] + 1
+            population[int(n)] += 1
 
-    max_value = max(population.keys())
-    for i in range(0, max_value + 1):
-        if i not in population:
-            population[i] = 0
+        days_gone = 0
+        while days_gone < 80:
+            new_population = [0] * 9
+            
+            new_fish = population[0]
+            for i in range(1, 9):
+                new_population[i - 1] = population[i]
 
-    # -- now make the population evolve
-    days_gone = 0
-    while days_gone < 80:        
-        # -- number of new lanterfish
-        new_lanterfish = population[0]
+            new_population[6] += new_fish
+            new_population[8] += new_fish
 
-        # -- scale down the timer of all other lanterfish
-        new_population = {}        
-        for key in population:
-            if key > 0:
-                new_population[key - 1] = population[key]
+            population = new_population
+            days_gone += 1
 
-        new_population[0] = 0 if 0 not in new_population else new_population[0]
-        new_population[6] = new_lanterfish if 6 not in new_population else new_population[6] + new_lanterfish
-        new_population[8] = new_lanterfish if 8 not in new_population else new_population[8] + new_lanterfish
-        
-        population = new_population        
-        days_gone += 1
-
-    print(f"Result of part one: {sum(population.values())}")
-
+        # -- count how many fish
+        print(f"Result of part one: {sum(population)}")
+    
 # ------
-
-def part_two():
-    # population[i] := number of lanternfish that will reproduce after i days.
-    population = {}
     
+def part_two():
+    population = [0] * 9
+
     with open("input.txt", "r") as f:
+        # -- read input
         for n in f.read().split(","):
-            population[int(n)] = 1 if int(n) not in population else population[int(n)] + 1
+            population[int(n)] += 1
 
-    max_value = max(population.keys())
-    for i in range(0, max_value + 1):
-        if i not in population:
-            population[i] = 0
+        days_gone = 0
+        while days_gone < 256:
+            new_population = [0] * 9
+            
+            new_fish = population[0]
+            for i in range(1, 9):
+                new_population[i - 1] = population[i]
 
-    # -- now make the population evolve
-    days_gone = 0
-    while days_gone < 256:        
-        # -- number of new lanterfish
-        new_lanterfish = population[0]
+            new_population[6] += new_fish
+            new_population[8] += new_fish
 
-        # -- scale down the timer of all other lanterfish
-        new_population = {}        
-        for key in population:
-            if key > 0:
-                new_population[key - 1] = population[key]
+            population = new_population
+            days_gone += 1
 
-        new_population[0] = 0 if 0 not in new_population else new_population[0]
-        new_population[6] = new_lanterfish if 6 not in new_population else new_population[6] + new_lanterfish
-        new_population[8] = new_lanterfish if 8 not in new_population else new_population[8] + new_lanterfish
-        
-        population = new_population        
-        days_gone += 1
-
-    print(f"Result of part two: {sum(population.values())}")
+        # -- count how many fish
+        print(f"Result of part one: {sum(population)}")    
 
 # ------
     
