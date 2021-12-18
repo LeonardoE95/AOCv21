f5b9f7c546d4f7bdbe89370324009b488c82cea5
LeonardoE95 <leonardotamiano95@gmail.com>
2021-12-19 00:58:09 +0100

    Added brute-force way of solving day 08

--- 08/aoc_8.py
+++ 08/aoc_8.py
@@ -12,8 +12,6 @@ digit 6 uses 6 segments
 digit 7 uses 3 segments
 digit 8 uses 7 segments
 digit 9 uses 6 segments
-
-  
 """
 
 import itertools
@@ -58,83 +56,153 @@ SEGM2DIG = {
     'abcdfg': '9'
 }
 
+# def decode_mappings(input_values):
+#     # Initially this is unknown
+#     seg2wire = {
+#         'a': set(),
+#         'b': set(),
+#         'c': set(),
+#         'd': set(),
+#         'e': set(),
+#         'f': set(),
+#         'g': set(),
+#     }
+    
+#     wire_1 = ""
+#     wire_4 = ""
+#     wire_7 = ""
+#     wire_8 = ""
+    
+#     # --------------------------------
+#     # -- get wires of well known digit
+#     for val in input_values:
+#         if len(val) == 2:
+#             wire_1 = set(list(val))
+#         elif len(val) == 3:
+#             wire_7 = set(list(val))
+#         elif len(val) == 4:
+#             wire_4 = set(list(val))
+#         elif len(val) == 7:
+#             wire_8 = set(list(val))
+
+#     # --------------------------------
+#     # -- figure out which wire gets mapped to segment A
+#     seg2wire['a'] = wire_7 - wire_1
+
+#     # --------------------------------
+#     # -- figure out which wire gets mapped to segment C
+#     tmp = ""
+#     for val in input_values:
+#         if len(val) == 6:
+#             tmp += "".join(set(wire_8) - set(list(val)))
+#     seg2wire['c'] = wire_1.intersection(set(list(tmp)))
+
+#     # --------------------------------
+#     # -- figure out which wire gets mapped to segment F
+#     seg2wire['f'] = wire_7 - seg2wire['c'] - seg2wire['a']
+
+#     # --------------------------------
+#     # -- figure out which wire gets mapped to segment D
+#     known_values = set(list(seg2wire['a']) + list(seg2wire['c']) + list(seg2wire['f']))
+#     tmp = []
+#     for val in input_values:
+#         set_val = set(list(val))
+#         if len(set_val - known_values) == 2:
+#             for x in (set_val - known_values):
+#                 if x not in tmp:
+#                     tmp.append(x)
+#                 else:
+#                     seg2wire['d'] = set(x)
+
+#     # --------------------------------
+#     # -- figure out which wire gets mapped to segment B
+#     seg2wire['b'] = wire_4 - set(list(seg2wire['c']) + list(seg2wire['d']) + list(seg2wire['f']))
+
+#     # --------------------------------
+#     # -- figure out which wire gets mapped to segment G
+#     known_values = set(list(seg2wire['a']) + list(seg2wire['c']) + list(seg2wire['d']) + list(seg2wire['f']))
+#     for val in input_values:
+#         set_val = set(list(val)) - known_values
+#         if len(set_val) == 1 and list(set_val)[0] != list(seg2wire['b'])[0]:
+#             seg2wire['g'] = set_val
+
+#     # --------------------------------
+#     # -- figure out which wire gets mapped to segment E
+#     known_values = set([list(x)[0] for x in seg2wire.values() if len(x) > 0])
+#     seg2wire['e'] = set(seg2wire.keys()) - known_values
+
+#     result = [list(x)[0] for x in seg2wire.values()]
+#     return result
+
+def wire2seg(value, connections):
+    """This function takes a value of wires which are turned on and a set
+    of connection between wires and segments and returns the value of
+    the segments which are turned on.
+    
+    The value returned can be used to understand which digit is shown
+    with the given wires.
+
+    The connections are given with a list, where the index of the list
+    represents a wire, and the value of the list represents a
+    segment. For example the list
+
+    ['d', 'g', 'b', 'c', 'a', 'e', 'f']
+
+    represents that the following situation:
+
+    - The wire 'a' is used to turn on the segment 'd'
+    - The wire 'b' is used to turn on the segment 'g'
+    - The wire 'c' is used to turn on the segment 'b'
+    - The wire 'd' is used to turn on the segment 'c'
+    - The wire 'e' is used to turn on the segment 'a'
+    - The wire 'f' is used to turn on the segment 'e'
+    - The wire 'g' is used to turn on the segment 'f'
+
+    Thus, the sequence of wires 'cfbegad' is mapped to the sequence of
+    segments 'begafdc', which represents the digit '8'.
+
+    """
+
+    seg_str = ""
+
+    for c in list(value):
+        # go from the wire value to the segment value
+        seg_str += chr(ord('a') + connections.index(c))
+
+    return "".join(sorted(seg_str))
+
+
 def decode_mappings(input_values):
-    # Initially this is unknown
-    seg2wire = {
-        'a': set(),
-        'b': set(),
-        'c': set(),
-        'd': set(),
-        'e': set(),
-        'f': set(),
-        'g': set(),
-    }
+    values = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
     
-    wire_1 = ""
-    wire_4 = ""
-    wire_7 = ""
-    wire_8 = ""
-    
-    # --------------------------------
-    # -- get wires of well known digit
-    for val in input_values:
-        if len(val) == 2:
-            wire_1 = set(list(val))
-        elif len(val) == 3:
-            wire_7 = set(list(val))
-        elif len(val) == 4:
-            wire_4 = set(list(val))
-        elif len(val) == 7:
-            wire_8 = set(list(val))
+    for perm in itertools.permutations(values):
+        # check if using the given permutation is we're able to cover
+        # all of the digits with the given input vales.
+        
+        found_digits = {
+            'abcefg': False,   # 0
+            'cf': False,       # 1
+            'acdeg': False,    # 2
+            'acdfg': False,    # 3
+            'bcdf': False,     # 4
+            'abdfg': False,    # 5
+            'abdefg': False,   # 6
+            'acf': False,      # 7
+            'abcdefg': False,  # 8
+            'abcdfg': False    # 9
+        }
+        
+        for wire_str in input_values:
+            segment_str = wire2seg(wire_str, perm)
 
-    # --------------------------------
-    # -- figure out which wire gets mapped to segment A
-    seg2wire['a'] = wire_7 - wire_1
+            if segment_str not in found_digits:
+                # -- this perm is not valid
+                break
+            else:
+                found_digits[segment_str] = True
 
-    # --------------------------------
-    # -- figure out which wire gets mapped to segment C
-    tmp = ""
-    for val in input_values:
-        if len(val) == 6:
-            tmp += "".join(set(wire_8) - set(list(val)))
-    seg2wire['c'] = wire_1.intersection(set(list(tmp)))
-
-    # --------------------------------
-    # -- figure out which wire gets mapped to segment F
-    seg2wire['f'] = wire_7 - seg2wire['c'] - seg2wire['a']
-
-    # --------------------------------
-    # -- figure out which wire gets mapped to segment D
-    known_values = set(list(seg2wire['a']) + list(seg2wire['c']) + list(seg2wire['f']))
-    tmp = []
-    for val in input_values:
-        set_val = set(list(val))
-        if len(set_val - known_values) == 2:
-            for x in (set_val - known_values):
-                if x not in tmp:
-                    tmp.append(x)
-                else:
-                    seg2wire['d'] = set(x)
-
-    # --------------------------------
-    # -- figure out which wire gets mapped to segment B
-    seg2wire['b'] = wire_4 - set(list(seg2wire['c']) + list(seg2wire['d']) + list(seg2wire['f']))
-
-    # --------------------------------
-    # -- figure out which wire gets mapped to segment G
-    known_values = set(list(seg2wire['a']) + list(seg2wire['c']) + list(seg2wire['d']) + list(seg2wire['f']))
-    for val in input_values:
-        set_val = set(list(val)) - known_values
-        if len(set_val) == 1 and list(set_val)[0] != list(seg2wire['b'])[0]:
-            seg2wire['g'] = set_val
-
-    # --------------------------------
-    # -- figure out which wire gets mapped to segment E
-    known_values = set([list(x)[0] for x in seg2wire.values() if len(x) > 0])
-    seg2wire['e'] = set(seg2wire.keys()) - known_values
-
-    result = [list(x)[0] for x in seg2wire.values()]
-    return result
+        if sum(list(found_digits.values())) == 10:
+            return perm
 
 def part_two():
     with open("input.txt", "r") as f:
@@ -146,14 +214,9 @@ def part_two():
             mappings = decode_mappings(input_values)
             
             line_res = ''
-            for val in output_values:
-                decoded_str = ""
-                for c in list(val):
-                    decoded_str += chr(ord('a') + mappings.index(c))
-                str_l = list(decoded_str)
-                str_l.sort()
-                decoded_str = "".join(str_l)
-                line_res += SEGM2DIG[decoded_str]
+            for wire_str in output_values:
+                segment_str = wire2seg(wire_str, mappings)
+                line_res += SEGM2DIG[segment_str]
 
             final_result += int(line_res)
 
